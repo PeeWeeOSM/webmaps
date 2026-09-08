5ad01f40f5dc505d9e7b35bcc125d4bd815de21b
PeeWeeOSM <piewie32@mailbx.org>
2026-09-08 17:45:15 +0200

    webmaps opgeschoond

---
+++ GH_refresh_webmaps.sh
@@ -0,0 +1,6 @@
+
+
+cd /home/peewee/github/webmaps   # ga naar de juiste repo
+git add .                                 # voeg alle gewijzigde bestanden toe
+git commit -m "webmaps opgeschoond"   # sla op met een berichtje
+git push                             # stuur naar GitHub     
